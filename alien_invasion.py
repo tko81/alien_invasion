a1a002b51860ce24d4a659f2c9185f0ee5ff7395
tko81 <tko81@hotmail.com>
2022-05-27 19:38:36 +0800

    first step

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,76 @@
 import sys
 import pygame
+
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1600,900))
-        self.bg_color = (230,230,230)
+        self.settings = Settings()
+        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         while True:
-            for even in pygame.event.get():
-                if even.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.bg_color)
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
+            self._update_screen()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            if event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+            elif event.key == pygame.K_q:
+                sys.exit()
+            elif event.key == pygame.K_SPACE:
+
+                self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 1600
+        self.screen_height = 900
+        self.bg_color = (0,0,0)
+        self.ship_speed = 1.5
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullet_allowed = 5
